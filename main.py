b13c2afd2a77e98b6f9f9183b96f3398b747ce02
Alabi, Tobi Michael <56853529+Michael-HK@users.noreply.github.com>
2023-07-29 23:37:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from transformers import pipeline
+import json
+import requests
+from bs4 import BeautifulSoup
 
+@app.route('/predict', methods = ['POST'])
+def job_authentication(url):
+  r = requests.get(url)
+  soup = BeautifulSoup(r.text, 'html.parser')
+  req = []
+  tag = ['responsibilities', 'requirements', 'benefits', 'roles', 'job details']
+  for i in tag:
+    try:
+      regex = re.compile(f'.*{i}.*')
+      results = soup.find_all('p', {'class':regex})
+      req.append(results.text)
+    except Exception as e:
+      req.append('None')
+  # load the model
+  if len(req) > 2:
+    pt_model = AutoModelForSequenceClassification.from_pretrained("/transformer/pt_save_pretrained")
+    classifier = pipeline("text-classification", model="/transformer/pt_save_pretrained")
+    classifier(req)
+    return classifier[0]['label']
