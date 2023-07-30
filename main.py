64f12f2e4f3ea588321f7e515be7caaf5bea5f39
Alabi, Tobi Michael <56853529+Michael-HK@users.noreply.github.com>
2023-07-30 10:48:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from transformers import pipeline, AutoModelForSequenceClassification
 import json
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, jsonify
 
+app = Flask(__name__)
 @app.route('/predict', methods = ['POST'])
-def job_authentication(url):
-  r = requests.get(url)
+def job_authentication():
+  jsonfil = request.get_json()
+  url = dict(jsonfil)
+  r = requests.get(url.values())
   soup = BeautifulSoup(r.text, 'html.parser')
   req = []
   tag = ['responsibilities', 'requirements', 'benefits', 'roles', 'job details']
@@ -18,8 +22,15 @@ def job_authentication(url):
       req.append('None')
   # load the model
   if len(req) > len(tag):
-    pt_model = AutoModelForSequenceClassification.from_pretrained("/transformer/pt_save_pretrained")
-    classifier = pipeline("text-classification", model="/transformer/pt_save_pretrained")
+    #pt_model = AutoModelForSequenceClassification.from_pretrained("/transformer/pt_save_pretrained")
+    #classifier = pipeline("text-classification", model="/transformer/pt_save_pretrained")  #load the model once 
     classifier(req)
-    return classifier[0]['label']
-  return "Sufficient details are not available to determine if the job post is real or fake"
+    return jsonify(classifier[0]['label'])
+  return jsonify("Sufficient details are not available to determine if the job post is real or fake")
+
+
+if __name__ == '__main__':
+# Model is loaded when the API is launched
+	classifier = pipeline("text-classification", model="/transformer/pt_save_pretrained")
+	app.run(debug=True)
+  
